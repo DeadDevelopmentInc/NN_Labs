bb293f4e6a65d5925fb97e4d1dee623730d49aec
DeadDevelopmentInc <demidovmainmailbox@gmail.com>
2018-12-28 15:53:24 +0300

    Add solutions for 2,3,6 labs

---
+++ lab2/network.py
@@ -0,0 +1,10 @@
+class Network():
+
+
+    def __init__(self):
+    
+
+    def train(self, X, Y):
+    
+    def predict(self, X):
+        return predict
---
+++ lab3/main.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import xml.etree.ElementTree as ET
+from .svm import SVMTrainer, SVMPredictor
+from .kernel import Kernel
+
+def reading_datasets(path, num_class):
+    folders = os.listdir(path)
+    if len(folders) != num_class:
+        raise Exception("Invalid class number in datasets")
+    for folder in folders:
+    
+    return X, Y
+
+def main():
+    try:
+        if len(sys.argv[1:]) != 1:
+            raise Exception("Invalid command arguments")
+        tree = ET.parse(sys.argv[1:][0])
+        root = tree.getroot()
+        if len(root) != 1:
+            raise Exception("Invalid file, check file")
+        trainpath = root[0].attrib['name']
+        print("Start reading datasets")
+        X, Y = reading_datasets(trainpath, 3)
+        print("Finish reading datasets\nStart creating and training network")
+        
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    main()
