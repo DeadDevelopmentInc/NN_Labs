d7e431052876fab5510c3f6265e29dd5630d928e
DeadDevelopmentInc <demidovmainmailbox@gmail.com>
2018-12-29 14:26:03 +0300

    Finish lab 6

--- lab6/main.py
+++ lab6/main.py
@@ -1,60 +1,110 @@
 import sys
 import os
 import xml.etree.ElementTree as ET
-import keras 
+from keras.models import Sequential
+from keras.layers import  Dense
+from sklearn import svm 
+from scipy import misc
+import numpy as np
 
-def read_datasets(path, num_class):
-    folders = os.listdir(path)
-    if len(folders) != num_class:
-        raise Exception("Invalid class number in datasets")
-    for folder in folders:
-    
-    return X, Y
-
-def read_one_file(path_to_file):
+def thod(X, th = 125):
+    for i in range(len(X)):
+        if X[i] > th:
+            X[i] = 1
+        else:
+            X[i] = 0
     return X
 
-def train_network(model, X, Y, b_s):
-    model.fit(X, Y,epoch=20, batch_size = b_s)
-    return model
+def read_datasets(m_path, num_class):
+    folders = os.listdir(m_path)
+    X, Y = [], []
+    if len(folders) != num_class:
+        raise Exception("Invalid class number in datasets")
+    _class = 0
+    for folder in folders:
+        path = "/".join([m_path, folder])
+        print(path)
+        for file in os.listdir(path):
+            X.append(thod((misc.imread("/".join([path, file])))[:,:,:3].reshape(1875)))
+            if _class == 0:
+                ar = -1
+            if _class == 1:
+                ar = 0
+            if _class == 2:
+                ar = 1
+            Y.append(ar)
+        _class += 1
+    return np.asarray(X), np.asarray(Y)
 
-def train_svm(model, X, Y):
-    return model
+def reading_datasets(m_path, num_class):
+    folders = os.listdir(m_path)
+    X, Y = [], []
+    if len(folders) != num_class:
+        raise Exception("Invalid class number in datasets")
+    _class = 0
+    for folder in folders:
+        path = "/".join([m_path, folder])
+        print(path)
+        for file in os.listdir(path):
+            X.append(thod((misc.imread("/".join([path, file])))[:,:,:3].reshape(1875)))
+            if _class == 0:
+                ar = [1,0,0]
+            if _class == 1:
+                ar = [0,1,0]
+            if _class == 2:
+                ar = [0,0,1]
+            Y.append(ar)
+        _class += 1
+    return np.asarray(X), np.asarray(Y)
 
-def predict(model, X):
-    return model.predict(X)
+def read_one_file(path):
+    return np.array([np.asarray(thod((misc.imread(path))[:,:,:3].reshape(1875)))])
 
 def main():
-    
+    if len(sys.argv[1:]) != 1:
+        raise Exception("Invalid command arguments")
+    flag = False
+    while not flag:
+        print("Menu:\n1.NN with Tensorflow and Keras (lab2)\n2.SVM with scikit (lab3)\n3. Exit\nWrite choose")
+        choose = input()
+        if choose == "1":
+            main_NN(sys.argv[1:][0])
+        elif choose == "2":
+            main_SVM(sys.argv[1:][0])
+        else:
+            flag = True
 
 
-def main_SVM():
+def main_SVM(file):
     try:
-        if len(sys.argv[1:]) != 1:
-            raise Exception("Invalid command arguments")
-        tree = ET.parse(sys.argv[1:][0])
+        tree = ET.parse(file)
         root = tree.getroot()
-        if len(root) != 2:
+        if len(root) != 1:
             raise Exception("Invalid file, check file")
         trainpath = root[0].attrib['name']
-        input_shape = int(root[1].attrib["size"])
-        batch_size = int(root[2].attrib["size"])
         print("Start reading datasets")
         X, Y = read_datasets(trainpath, 3)
-        print("Finish reading datasets\nStart creating and training SVM")
-        model = train_svm(model ,X, Y)
-        #training
+        print("Finish reading datasets\nStart creating and training network")
+        trainer = svm.SVC(gamma='scale')
+        trainer.fit(X, Y)
         flag_out = False
-        while !flag-out:
-            print("Choose option from list (write number of option)\n1.Recognize image\n2.Re-train SVM\n3.Exit ")
-            choose = read()
+        while not flag_out:
+            print("===========================================\nChoose option from list (write number of option)\n1.Recognize image\n2.Re-train network\n3.Exit ")
+            choose = input()
             if choose == "1":
                 print("Write path to image: ")
-                pred = read_one_file(read())
-                print(predict(model, pred))
+                pred = read_one_file(input())
+                pred = trainer.predict(pred)
+                #print(pred, score)
+                if pred == -1 :
+                    print("Your class is Eyes")
+                elif pred == 0:
+                    print("Your class is Mouth")
+                else: 
+                    print("Your class is Nose")
             elif choose == "2":
                 X, Y = read_datasets(trainpath, 3)
-                model = train_svm(model ,X, Y)
+                trainer.fit(X, Y)
             else:
                 flag_out = True
         
@@ -62,41 +112,44 @@ def main_SVM():
         print(ex)
 
 
-def main_NN():
+def main_NN(file):
     try:
-        if len(sys.argv[1:]) != 1:
-            raise Exception("Invalid command arguments")
-        tree = ET.parse(sys.argv[1:][0])
+        tree = ET.parse(file)
         root = tree.getroot()
-        if len(root) != 2:
+        if len(root) != 1:
             raise Exception("Invalid file, check file")
         trainpath = root[0].attrib['name']
-        input_shape = int(root[1].attrib["size"])
-        batch_size = int(root[2].attrib["size"])
         print("Start reading datasets")
-        X, Y = read_datasets(trainpath, 3)
+        X, Y = reading_datasets(trainpath, 3)
         print("Finish reading datasets\nStart creating and training network")
         model = Sequential()
-        model.add(Dense(input_shape, input_dim=input_shape,  activation='tanh'))
+        model.add(Dense(X.shape[1], input_dim=X.shape[1],  activation='tanh'))
         model.add(Dense(144,  activation='tanh'))
         model.add(Dense(72,  activation='tanh'))
         model.add(Dense(66,  activation='tanh'))
         model.add(Dense(33,  activation='tanh'))
         model.add(Dense(3, activation='sigmoid'))
         model.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
-        model = train_network(model ,X, Y, bacth_size)
+        model.fit(X, Y, batch_size = 10, epochs=10)
         #training
         flag_out = False
-        while !flag-out:
+        while not flag_out:
             print("Choose option from list (write number of option)\n1.Recognize image\n2.Re-train network\n3.Exit ")
-            choose = read()
+            choose = input()
             if choose == "1":
                 print("Write path to image: ")
-                pred = read_one_file(read())
-                print(predict(model, pred))
+                pred = read_one_file(input())
+                pred = model.predict(pred)
+                max_val = np.argmax(pred[0])
+                if max_val == 0:
+                    print("Your class is Eyes")
+                elif max_val == 1:
+                    print("Your class is Mouth")
+                else: 
+                    print("Your class is Nose")
             elif choose == "2":
                 X, Y = read_datasets(trainpath, 3)
-                model = train_network(model ,X, Y, bacth_size)
+                model.fit(X, Y, bacth_size = 10)
             else:
                 flag_out = True
         
